794433ae871185af5554e7a964f78b66ad4833a6
Alfredo Quevedo <aquevedo@unc.edu.ar>
2025-06-12 12:46:39 -0300

    Updated docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,16 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://alfredoq.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/TidyScreen_v2_docs_new/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
+  organizationName: 'alfredoq', // Usually your GitHub org/user name.
+  projectName: 'TidyScreen_v2_docs', // Usually your repo name.
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
 
